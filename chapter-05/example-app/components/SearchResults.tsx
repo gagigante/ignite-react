f83f42d18699d6589b526bdab37e6a90b7d13ba3
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-01-24 21:44:40 -0300

    chapter 05: performance - useMemo

--- chapter-05/example-app/components/SearchResults.tsx
+++ chapter-05/example-app/components/SearchResults.tsx
@@ -1,4 +1,6 @@
-import { ProductItem } from "./ProductItem";
+import { useMemo } from 'react';
+
+import { ProductItem } from './ProductItem';
 
 type SearchResultsProps = {
   results: Array<{
@@ -9,8 +11,16 @@ type SearchResultsProps = {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((acc, item) => {
+      return acc + item.price
+    }, 0)
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => {
         // eslint-disable-next-line react/jsx-key
         return <ProductItem product={product}/>;
@@ -18,3 +28,17 @@ export function SearchResults({ results }: SearchResultsProps) {
     </div>
   )
 }
+
+/**
+ * useMemo 
+ * 
+ * -> Função que memoriza (memoization) um valor e evita que ele seja montado do zero caso seu array de dependências não mude
+ * -> Conserva o valor no mesmo endereço permitindo a comparação referencial
+ */
+
+/**
+ * Quando devo utilizar?
+ * 
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial (Quando repassa a informação para um componente filho)
+ */
