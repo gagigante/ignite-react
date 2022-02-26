9bae7c94a23c66e83f222e96907bfd9a4f2e9f08
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-02-26 11:30:36 -0300

    chapter 04: declarative-ui - sidebar component

--- chapter-04/declarative-ui-app/src/pages/dashboard.tsx
+++ chapter-04/declarative-ui-app/src/pages/dashboard.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from 'next'
+import { Flex } from '@chakra-ui/react'
 
 import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
 
 const Dashboard: NextPage = () => {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
 
