a109845441da4bd0550fd5a2552381b1669b452e
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-03-02 09:08:03 -0300

    chapter 04: declarative-ui - pagination component

--- chapter-04/declarative-ui-app/src/pages/users/index.tsx
+++ chapter-04/declarative-ui-app/src/pages/users/index.tsx
@@ -14,7 +14,9 @@ import {
   Text 
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
 import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
@@ -84,6 +86,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
