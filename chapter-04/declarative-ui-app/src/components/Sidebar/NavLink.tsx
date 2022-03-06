f0721c303df98044d2934a36857fa158e18dbdff
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-03-06 19:48:28 -0300

    chapter 04: declarative-ui - active link state

--- chapter-04/declarative-ui-app/src/components/Sidebar/NavLink.tsx
+++ chapter-04/declarative-ui-app/src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import { Icon, Link as ChackraLink, Text, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
-import { ElementType, ReactNode } from 'react'
-import Link from 'next/link'
+import { ElementType } from 'react'
+
+import { ActiveLink } from '../ActiveLink'
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -10,11 +11,11 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChackraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">{children}</Text>
       </ChackraLink>
-    </Link>
+    </ActiveLink>
   )
 }
